2f8a4b9f8cc87badbeb4bba65a536dfc02ea3c7b
Carlos Diaz-Padron <carlosdiazpadron@gmail.com>
2024-02-22 14:21:54 -0500

    Initial Commit

---
+++ HotOrBot/HotOrBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotOrBotApp.swift
+//  HotOrBot
+//
+//  Created by Carlos on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct HotOrBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
